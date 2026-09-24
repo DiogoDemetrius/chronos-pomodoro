22744024716f8133f9d32fac5424895d4c32b9dc
Carlos Henrique de Azevedo Moreira <diogod.mmoreira@gmail.com>
2026-09-24 13:40:06 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./styles/global.css";
+import "./styles/theme.css";
+
+export function App() {
+  console.log("Oi");
+
+  return (
+    <div>
+      <h1>Olá mundo</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+        provident labore cupiditate vero nesciunt temporibus enim ratione!
+        Tenetur error deserunt delectus qui amet quos, itaque cum, et veritatis
+        magni at.
+      </p>
+    </div>
+  );
+}
