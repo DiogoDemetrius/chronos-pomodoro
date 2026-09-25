ff567ca6c382305c6c0d37ad1b5aa24d40d829a0
Carlos Henrique de Azevedo Moreira <diogod.mmoreira@gmail.com>
2026-09-25 12:22:16 -0300

    components Container e Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
-  console.log("Oi");
-
   return (
-    <div>
-      <h1>Olá mundo</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
-        provident labore cupiditate vero nesciunt temporibus enim ratione!
-        Tenetur error deserunt delectus qui amet quos, itaque cum, et veritatis
-        magni at.
-      </p>
-    </div>
+    <>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+    </>
   );
 }
